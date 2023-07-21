d8eebb51a29e4d588cd8ad15339e4183ddfc167e
Abdulbassit Abdullahi <funabab@gmail.com>
2023-07-22 00:05:29 +0100

    feat: dashboard record page

--- packages/frontend/src/pages/dashboard/record/index.tsx
+++ packages/frontend/src/pages/dashboard/record/index.tsx
@@ -1,9 +1,15 @@
 import React, { useEffect } from 'react'
 import {
+  Box,
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
+  Button,
   Container,
+  Flex,
+  SimpleGrid,
+  Stack,
+  Text,
 } from '@chakra-ui/react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useDocumentData } from 'react-firebase-hooks/firestore'
@@ -69,6 +75,162 @@ const DashboardRecordPage: React.FC<Props> = () => {
             </BreadcrumbLink>
           </BreadcrumbItem>
         </Breadcrumb>
+
+        <Flex
+          columnGap={10}
+          justifyContent="space-between"
+          rowGap={10}
+          pt="10"
+          direction={{ base: 'column', xl: 'row' }}
+        >
+          <Box order={{ base: 2, xl: 'unset' }}>
+            <SimpleGrid
+              columns={2}
+              w="max-content"
+              gap="5"
+              p="5"
+              border="1px"
+              borderColor="gray.400"
+              rounded="sm"
+            >
+              <Text as="strong">Age</Text>
+              <Text as="p" textAlign="right">
+                {record.age}
+              </Text>
+
+              <Text as="strong">Gender</Text>
+              <Text as="p" textAlign="right">
+                {{ '1': 'Male', '2': 'Female' }[record.gender]}
+              </Text>
+            </SimpleGrid>
+
+            <SimpleGrid
+              columns={2}
+              w="max-content"
+              mt="5"
+              gap="5"
+              p="5"
+              border="1px"
+              borderColor="gray.400"
+              rounded="sm"
+            >
+              <Text as="strong">Direct Bilirubin</Text>
+              <Text as="p" textAlign="right">
+                {record.directBilirubin}
+              </Text>
+
+              <Text as="strong">Total Bilirubin</Text>
+              <Text as="p" textAlign="right">
+                {record.totalBilirubin}
+              </Text>
+            </SimpleGrid>
+
+            <SimpleGrid
+              columns={2}
+              w="max-content"
+              mt="5"
+              gap="5"
+              p="5"
+              border="1px"
+              borderColor="gray.400"
+              rounded="sm"
+            >
+              <Text as="strong">Alkaline Phosphotase</Text>
+              <Text as="p" textAlign="right">
+                {record.alkalinePhosphotase}
+              </Text>
+
+              <Text as="strong">Alamine Aminotransferase</Text>
+              <Text as="p" textAlign="right">
+                {record.alamineAminotransferase}
+              </Text>
+
+              <Text as="strong">Aspartate Aminotransferase</Text>
+              <Text as="p" textAlign="right">
+                {record.aspartateAminotransferase}
+              </Text>
+            </SimpleGrid>
+
+            <SimpleGrid
+              columns={2}
+              w="max-content"
+              mt="5"
+              gap="5"
+              p="5"
+              border="1px"
+              borderColor="gray.400"
+              rounded="sm"
+            >
+              <Text as="strong">Albumin</Text>
+              <Text as="p" textAlign="right">
+                {record.albumin}
+              </Text>
+
+              <Text as="strong">Aspartate Aminotransferase</Text>
+              <Text as="p" textAlign="right">
+                {record.aspartateAminotransferase}
+              </Text>
+
+              <Text as="strong">Total Protiens</Text>
+              <Text as="p" textAlign="right">
+                {record.totalProtiens}
+              </Text>
+            </SimpleGrid>
+          </Box>
+
+          <Stack alignItems={{ base: 'start', xl: 'end' }}>
+            <Button colorScheme="green" variant="outline">
+              Edit Record
+            </Button>
+
+            <SimpleGrid
+              columns={2}
+              w="max-content"
+              mt="5"
+              gap="3"
+              p="5"
+              border="1px"
+              borderColor="gray.400"
+              borderStyle="dashed"
+              rounded="sm"
+              alignItems="center"
+            >
+              <Text as="strong">Evaluation</Text>
+              <Text
+                as="p"
+                textAlign="right"
+                textTransform="capitalize"
+                fontSize="2xl"
+                color={
+                  record.status === 'negative'
+                    ? 'red.700'
+                    : record.status === 'positive'
+                    ? 'green.700'
+                    : 'gray.700'
+                }
+              >
+                {record.status}
+              </Text>
+              {false && (
+                <React.Fragment>
+                  <Text as="strong">Accuracy</Text>
+                  <Text as="p" textAlign="right" fontSize="2xl">
+                    70.00%
+                  </Text>
+                </React.Fragment>
+              )}
+              <Button
+                gridRowStart={5}
+                gridColumnStart={1}
+                gridColumnEnd={3}
+                size="lg"
+                colorScheme="green"
+              >
+                Prediction
+              </Button>
+            </SimpleGrid>
+          </Stack>
+        </Flex>
       </Container>
     </React.Fragment>
   )
