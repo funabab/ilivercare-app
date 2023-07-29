a12dea5dc3a5de15a849782c789245f1b97df51b
Abdulbassit Abdullahi <funabab@gmail.com>
2023-07-29 09:25:31 +0100

    chore: update app release name for new release builds

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    target: 'es6',
+  },
+  plugins: [
+    react(),
+    legacy({
+      targets: [
+        'chrome >= 64',
+        'edge >= 79',
+        'safari >= 11.1',
+        'firefox >= 67',
+      ],
+      ignoreBrowserslistConfig: true,
+      renderLegacyChunks: true,
+      modernPolyfills: ['es/global-this'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
